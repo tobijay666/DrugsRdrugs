a632f5889492e3a92c704ac2d8368de28bb3c29e
Menaka Anton <menakaantonjayakody@gmail.com>
2024-11-27 20:45:30 +0530

    first commit

---
+++ temp.sh
@@ -0,0 +1,12 @@
+
+# Create subfolders
+mkdir frontend backend
+
+# Create required files
+touch startup.sh up.sh compose.yaml ReadMe.md DevNotes.md
+
+# Add executable permissions to shell scripts
+chmod +x startup.sh up.sh
+
+# Provide a basic message
+echo "Project structure initialized successfully!"
